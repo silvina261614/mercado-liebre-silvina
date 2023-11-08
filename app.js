12562a59973ecd26651818f8156c49232dcd0fa9
silvina261614 <silvinaff@hotmail.com>
2023-11-08 00:00:57 -0300

    modificacion

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3007;
+
 
 const path = require('path');
 
 app.use('/static', express.static(__dirname + '/public'))
 
-app.listen(PORT, ()=>{
-    console.log("Servidor iniciado en el puerto " + PORT);
-});
+const port = process.env.PORT || 3007;
+app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'));
 
 app.get('/', (req, res)=>res.sendFile(__dirname + "/views/home.html"))
 
